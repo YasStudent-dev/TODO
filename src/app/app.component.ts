cda7fd8011303d861ca9b52c6ed11162b9e15c8e
YasStudent-dev <m.yasin.student@outlook.com>
2025-07-30 09:41:14 +0200

    Updated README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { TopNavbarComponent } from '@app/components/top-navbar/top-navbar.component';
 import { TodoComponent } from '@app/components/todo/todo.component';
 import { Title, Meta } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
+import { environment } from '@env';
 
 @Component({
     selector: 'app-root',
@@ -12,7 +13,11 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
-	constructor(private translate: TranslateService, private title: Title, private meta: Meta) {}
+	constructor(private translate: TranslateService, private title: Title, private meta: Meta) {
+        console.log("get env");
+        console.log(isDevMode());
+        console.log(environment.production);
+    }
 
     onInit() {
         this.translate.get(['app.metadata.title', 'app.metadata.description']).subscribe((res: any) => {
